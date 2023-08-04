8399ffb96e9400433f58aeb049790e1cec2bf53a
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-08-04 21:42:38 +0100

    message

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import { MdClose } from "react-icons/md";
 import "../css/Ticket.css";
-import { Link } from "react-router-dom";
 import { IoMdCheckmark } from "react-icons/io";
+import { MdArrowForwardIos, MdArrowBackIos } from "react-icons/md";
 
 const Modal = ({ show, handleShow }) => {
   let [click1, setClick1] = useState(true);
   let [click2, setClick2] = useState(true);
   let [count, setCount] = useState(0);
+
+  const [comp, setComp] = useState(true);
 
   const handleClick1 = () => {
     if (click1 === true) {
@@ -39,50 +41,83 @@ const Modal = ({ show, handleShow }) => {
         <div className="mod-container">
           <h6>SELECT TICKETS TO TRANSFER</h6>
           <div className="line"></div>
-          <div className="imark">
-            <span>i</span>
-            <p>
-              Only transfer tickets to people you know and trust to ensure
-              everyone stays safe and socially distanced.
-            </p>
-          </div>
-          <div className="floor">
-            <p>Sec Floor, Row GA</p>
-            <span>
-              <iconify-icon icon="ion:ticket-sharp"></iconify-icon>&nbsp;2
-              tickets
-            </span>
-          </div>
-          <div className="seat">
-            <div>
-              <p>SEAT-</p>
-              <span
-                onClick={handleClick1}
-                className={`${click1 ? "unclicked" : "clicked"}`}
-              >
-                <IoMdCheckmark />
-              </span>
-            </div>
-            <div>
-              <p>SEAT-</p>
-              <span
-                onClick={handleClick2}
-                className={`${click2 ? "unclicked" : "clicked"}`}
-              >
-                <IoMdCheckmark />
-              </span>
-            </div>
-          </div>
-          <div className="line"></div>
-          <div className="floor top">
-            <p>{count} Selected</p>
-            <span>
-              <Link to="">
-                TRANSFER T0
-                <iconify-icon icon="dashicons:arrow-right-alt2"></iconify-icon>
-              </Link>
-            </span>
-          </div>
+          {comp ? (
+            <>
+              <div className="imark">
+                <span>i</span>
+                <p>
+                  Only transfer tickets to people you know and trust to ensure
+                  everyone stays safe and socially distanced.
+                </p>
+              </div>
+              <div className="floor">
+                <p>Sec Floor, Row GA</p>
+                <span>
+                  <iconify-icon icon="ion:ticket-sharp"></iconify-icon>&nbsp;2
+                  tickets
+                </span>
+              </div>
+              <div className="seat">
+                <div>
+                  <p>SEAT-</p>
+                  <span
+                    onClick={handleClick1}
+                    className={`${click1 ? "unclicked" : "clicked"}`}
+                  >
+                    <IoMdCheckmark />
+                  </span>
+                </div>
+                <div>
+                  <p>SEAT-</p>
+                  <span
+                    onClick={handleClick2}
+                    className={`${click2 ? "unclicked" : "clicked"}`}
+                  >
+                    <IoMdCheckmark />
+                  </span>
+                </div>
+              </div>
+              <div className="line"></div>
+              <div className="floor top">
+                <p>{count} Selected</p>
+                <span className="transfer-to" onClick={() => setComp(false)}>
+                  <span>TRANSFER T0</span>
+                  <MdArrowForwardIos />
+                </span>
+              </div>
+            </>
+          ) : (
+            <>
+              <form action="" className="modal-form">
+                <p>{count} Ticket Selected</p>
+                <span className="span">
+                  Sec
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  Row
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  Seat
+                </span>
+                <label htmlFor="">First Name</label>
+                <input type="text" placeholder="First Name" />
+                <label htmlFor="">Last Name</label>
+                <input type="text" placeholder="Last Name" />
+                <label htmlFor="">Email or Mobile Number</label>
+                <input type="text" placeholder="Email or Mobile Number" />
+                <label htmlFor="">Note</label>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <div className="line"></div>
+                <div className="floor top">
+                  <span className="transfer-to" onClick={() => setComp(true)}>
+                    <MdArrowBackIos />
+                    <span>TRANSFER T0</span>
+                  </span>
+                  <button className="transfer-form-btn">
+                    Transfer {count} {count > 1 ? "Tickets" : "Ticket"}
+                  </button>
+                </div>
+              </form>
+            </>
+          )}
           <MdClose className="close-icon" onClick={handleShow} />
         </div>
       </div>
