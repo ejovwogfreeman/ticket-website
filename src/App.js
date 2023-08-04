aaf42b18449af04d2befd4dfe0f25a77b77dddf7
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-08-04 12:56:54 +0100

    message

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Ticket from "./components/Ticket";
-// import Scroll from "./components/Scroll";
+import Tickets from "./components/Tickets";
+import Edit from "./components/Edit";
+import Create from "./components/Create";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Ticket />
-        {/* <Scroll /> */}
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <form>
+      <label htmlFor="">EMAIL</label>
+      <input type="email" name="" id="" placeholder="Enter your email" />
+      <label htmlFor="">PASSWORD</label>
+      <input type="password" name="" id="" placeholder="Enter your password" />
+    </form>
+  );
+};
+
+export default Login;
