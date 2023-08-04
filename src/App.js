e33963e5f231e97c70e1ec5ffc3e723fad837480
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-08-04 13:01:42 +0100

    message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Ticket from "./components/Ticket";
-import Tickets from "./components/Tickets";
-import Edit from "./components/Edit";
-import Create from "./components/Create";
-import Login from "./components/Login";
+// import Tickets from "./components/Tickets";
+// import Edit from "./components/Edit";
+// import Create from "./components/Create";
+// import Login from "./components/Login";
 
 function App() {
   return (
