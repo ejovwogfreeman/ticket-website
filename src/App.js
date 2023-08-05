783ed2ec8d079ce5f422328f6ea915049d275dfc
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-08-05 04:30:40 +0100

    message

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Ticket from "./components/Ticket";
-// import Tickets from "./components/Tickets";
-// import Edit from "./components/Edit";
-// import Create from "./components/Create";
-// import Login from "./components/Login";
+import Tickets from "./components/Tickets";
+import Edit from "./components/Edit";
+import Create from "./components/Create";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Ticket />
+        <Routes>
+          <Route path="/" element={<Ticket />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/tickets" element={<Tickets />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
