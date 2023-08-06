f3243a1cd9fc6e1395ec6c5296470bdbc0de8364
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-08-06 14:11:16 +0100

    message

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,20 @@ import { Link } from "react-router-dom";
 import "../css/Navbar.css";
 
 const Navbar = () => {
+  const logout = () => {
+    localStorage.removeItem("user");
+    window.location.reload();
+  };
+
   return (
     <nav className="navBar">
       <Link to="/">TICKETS</Link>
       <Link to="/create">CREATE</Link>
       {/* <Link to="/edit">EDIT</Link> */}
       <Link to="/ticket">TICKET</Link>
-      <span>LOGOUT</span>
+      <span onClick={logout} style={{ cursor: "pointer" }}>
+        LOGOUT
+      </span>
     </nav>
   );
 };
