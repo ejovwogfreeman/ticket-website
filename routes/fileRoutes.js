dff14f9fd036da3e48e89ac9d06632267cbdc73f
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-08-06 14:54:05 +0100

    message

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -16,36 +16,36 @@
 
 // module.exports = router;
 
-const express = require("express");
-const mongoose = require("mongoose");
-const gridfs = require("gridfs-stream");
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const gridfs = require("gridfs-stream");
 
-const router = express.Router();
+// const router = express.Router();
 
-const conn = mongoose.connection;
-let gfs;
+// const conn = mongoose.connection;
+// let gfs;
 
-conn.once("open", () => {
-  gfs = gridfs(conn.db, mongoose.mongo);
-});
+// conn.once("open", () => {
+//   gfs = gridfs(conn.db, mongoose.mongo);
+// });
 
-router.get("file/:filename", async (req, res) => {
-  try {
-    if (!gfs) {
-      return res.status(500).json({ error: "GridFS is not initialized" });
-    }
+// router.get("file/:filename", async (req, res) => {
+//   try {
+//     if (!gfs) {
+//       return res.status(500).json({ error: "GridFS is not initialized" });
+//     }
 
-    const file = await gfs.files.findOne({ filename: req.params.filename });
-    if (!file) {
-      return res.status(404).json({ error: "File not found" });
-    }
+//     const file = await gfs.files.findOne({ filename: req.params.filename });
+//     if (!file) {
+//       return res.status(404).json({ error: "File not found" });
+//     }
 
-    const readStream = gfs.createReadStream({ filename: req.params.filename });
-    readStream.pipe(res);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error retrieving file" });
-  }
-});
+//     const readStream = gfs.createReadStream({ filename: req.params.filename });
+//     readStream.pipe(res);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Error retrieving file" });
+//   }
+// });
 
-module.exports = router;
+// module.exports = router;
--- server.js
+++ server.js
@@ -19,7 +19,41 @@ app.use(
 
 const port = process.env.PORT || 5000;
 
-app.use("/api/files/", require("./routes/fileRoutes"));
+// const express = require("express");
+const mongoose = require("mongoose");
+const gridfs = require("gridfs-stream");
+
+// const router = express.Router();
+
+const conn = mongoose.connection;
+let gfs;
+
+conn.once("open", () => {
+  gfs = gridfs(conn.db, mongoose.mongo);
+});
+
+app.get("/file/:filename", async (req, res) => {
+  try {
+    if (!gfs) {
+      return res.status(500).json({ error: "GridFS is not initialized" });
+    }
+
+    const file = await gfs.files.findOne({ filename: req.params.filename });
+    if (!file) {
+      return res.status(404).json({ error: "File not found" });
+    }
+
+    const readStream = gfs.createReadStream({ filename: req.params.filename });
+    readStream.pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error retrieving file" });
+  }
+});
+
+// module.exports = router;
+
+// app.use("/api/files/", require("./routes/fileRoutes"));
 app.use("/api/auth/", require("./routes/authRoutes"));
 app.use("/api/users/", require("./routes/userRoutes"));
 app.use("/api/ticket/", require("./routes/ticketRoutes"));
