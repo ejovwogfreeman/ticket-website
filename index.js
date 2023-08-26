df35f5230b1f9f836c93ab34032b44dc469666f6
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-08-26 13:39:47 +0100

    messaage

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-// const fetchTicket = async () => {
-//   const res = await fetch(`https://ticket-website.onrender.com/api/ticket/`);
-//   const data = await res.json();
-
-//   console.log(data);
-
-//   let html = "";
-
-//   data.forEach((x) => {
-//     html += `
-//       <li>http://127.0.0.1:5500/ticket.html/?id=${x._id}</li>
-//     `;
-//   });
-
-//   tickets.innerHTML = html;
-//   console.log(html);
-// };
-
-// let tickets = document.getElementById("tickets");
-
-// fetchTicket();
-
 const fetchTicket = async () => {
   const res = await fetch(`https://ticket-website.onrender.com/api/ticket/`);
   const data = await res.json();
